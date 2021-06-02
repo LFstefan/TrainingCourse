cfddda682c0f6ca1f9ba1fe9b22698f1d0ff25a4
liufei <1158545616@qq.com>
2021-06-02 10:18:56 +0800

    heap sort adjust

--- c++/algorithm/HeapSort.cpp
+++ c++/algorithm/HeapSort.cpp
@@ -10,16 +10,17 @@ void swap(int &n, int &m)
 
 void heapAdjust(int arr[], int start, int end)
 {
-  std::cout << "调整起始值和边界结束值：" << start << "," << end << std::endl;
+  // std::cout << "调整起始值和边界结束值：" << start << "," << end << std::endl;
   for (size_t i = start; (i >= 0 && i <= end); i--)
   {
-    std::cout << "被调增节点: " << i << std::endl;
+    // std::cout << "被调增节点: " << i << std::endl;
     int left = 2 * i + 1;
     int right = left + 1;
     if (right <= end)
     {
       int p = arr[left] > arr[right] ? left : right;
-      swap(arr[i], arr[p]);
+      if (arr[i] < arr[p])
+        swap(arr[i], arr[p]);
     }
     else if (left <= end)
     {
@@ -32,24 +33,24 @@ void heapAdjust(int arr[], int start, int end)
 }
 int main()
 {
-  int arr[] = {234, 654, 23, 264, 42, 435, 54356, 628, 54, 745};
+  int arr[] = {234, 654, 23, 264, 42678, 435, 54356, 628, 54, 745};
   int arr_size = sizeof(arr) / sizeof(arr[0]);
-  std::cout << "数组大小为：" << arr_size << std::endl;
+  // std::cout << "数组大小为：" << arr_size << std::endl;
   int n = arr_size - 1;
   while (n >= 0 && n < arr_size)
   {
     heapAdjust(arr, n / 2, n);
-    for (size_t i = 0; i < arr_size; i++)
-    {
-      std::cout << arr[i] << ",";
-    }
-    std::cout << "数组调整后的大顶堆" << std::endl;
+    // for (size_t i = 0; i < arr_size; i++)
+    // {
+    // std::cout << arr[i] << ",";
+    // }
+    // std::cout << "数组调整后的大顶堆" << std::endl;
     swap(arr[0], arr[n]);
-    for (size_t i = 0; i < arr_size; i++)
-    {
-      std::cout << arr[i] << ",";
-    }
-    std::cout << "首位调换位置后：" << std::endl;
+    // for (size_t i = 0; i < arr_size; i++)
+    // {
+    // std::cout << arr[i] << ",";
+    // }
+    // std::cout << "首位调换位置后：" << std::endl;
     n--;
   }
   for (size_t i = 0; i < arr_size; i++)
