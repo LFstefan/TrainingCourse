47d11576f7e7cf6a883e104c8af52b7a31fe747c
liufei <1158545616@qq.com>
2021-06-10 14:43:16 +0800

    add merge sort non-recursive version impl

--- java/algorithm/MergeSort.java
+++ java/algorithm/MergeSort.java
@@ -1,4 +1,3 @@
-import java.awt.*;
 import java.util.Arrays;
 
 public class MergeSort {
@@ -7,7 +6,11 @@ public class MergeSort {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(target));
-        recursiveVersion(0, target.length-1);
+//        recursiveVersion(0, target.length-1);
+//        System.out.println(Arrays.toString(target));
+
+        System.out.println("非递归迭代版本");
+        nonRecursiveVersion();
         System.out.println(Arrays.toString(target));
     }
 
@@ -33,15 +36,42 @@ public class MergeSort {
             tmp[p++] = target[l_start++];
         while (r_start <= r_end)
             tmp[p++] = target[r_start++];
-        for (p = start; p <= end; p++) {
+        for (p = start; p <= end; p++)
             target[p] = tmp[p];
-        }
     }
 
     /**
      * 非递归迭代版本
      */
-    public static void nonRecursiveVersion (String args) {
+    public static void nonRecursiveVersion () {
+        int len = target.length;
+        int block = 2;
+        while (block < len) {
+            for (int i = 0; i < len; i += block) {
+                merge(i, Math.min(len-1, (i+block-1)));
+            }
+            block = block<<1;
+        }
+        merge(0, len - 1);
+    }
 
+    /**
+     * function
+     */
+    public static void merge (int start, int end) {
+        int l_start = start;
+        int l_end = ((end - start)>>1) + start;
+        int r_start = l_end + 1;
+        int r_end = end;
+
+        int p = start;
+        while (l_start <= l_end && r_start <= r_end)
+            tmp[p++] = target[l_start] > target[r_start] ? target[r_start++] : target[l_start++];
+        while (l_start <= l_end)
+            tmp[p++] = target[l_start++];
+        while (r_start <= r_end)
+            tmp[p++] = target[r_start++];
+        for (p = start; p <= end; p++)
+            target[p] = tmp[p];
     }
 }
